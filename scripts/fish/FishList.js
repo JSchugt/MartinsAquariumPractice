9db29009aa5dfa98c774d8922461edacb7fa9047
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-03-03 15:23:54 -0600

    Made Fish Section Headers white to contrast the backgroud

--- scripts/fish/FishList.js
+++ scripts/fish/FishList.js
@@ -7,21 +7,23 @@ export const FishList = () =>{
 
     const DOMLocation = document.querySelector("#fishList");
 
-    let fishHTMLRep = ``;
+    let fishHTMLRep = "<section class=\"fishSection\"><h2 class=\"fishHead\">Worthy Fish</h2><div class=\"theFish\">";
     for(const onThingFromTheSea of holyFish){
         fishHTMLRep += fish(onThingFromTheSea);
     }
-    
+    fishHTMLRep += "</div></section><section class=\"fishSection\"><h2 class=\"fishHead\" >Soldier Fish</h2><div class=\"theFish\">";
     const soldier = getSoldierFish();
     for(const onThingFromTheSea of soldier){
         fishHTMLRep += fish(onThingFromTheSea);
     }
     
+    fishHTMLRep += "</div></section><section class=\"fishSection>\"><h2 class=\"fishHead\">Unworthy Fish</h2><div class=\"theFish\">";
 
     const unworthy = getUnworthy();
     for(const onThingFromTheSea of unworthy){
         fishHTMLRep += fish(onThingFromTheSea);
     }
+    fishHTMLRep += "</div></section>";
 
     DOMLocation.innerHTML = fishHTMLRep;
 }
